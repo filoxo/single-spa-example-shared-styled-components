570226500092240f78a9d8f032aa1bca3049a09f
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-09 14:03:49 -0600

    Generate styleguide utility module

---
+++ styleguide/webpack.config.js
@@ -0,0 +1,14 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "filoxo",
+    projectName: "styleguide",
+    webpackConfigEnv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
