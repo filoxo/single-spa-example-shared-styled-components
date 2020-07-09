69caa41709e309fe919dcde7ccee62b0618d9512
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-09 16:02:41 -0600

    Use styleguide Button component

--- navbar/src/root.component.js
+++ navbar/src/root.component.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Button } from "@filoxo/styleguide";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <div>
+      Navbar
+      <div>
+        <Button>Log in</Button>
+      </div>
+    </div>
+  );
 }
