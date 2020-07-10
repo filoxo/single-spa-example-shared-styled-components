42b13bc7d3211fb3a279ca11db560b24845022fa
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-10 15:33:02 -0600

    Add some interactivity to navbar

--- navbar/src/root.component.js
+++ navbar/src/root.component.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@filoxo/styleguide";
 import styles from "./root.styles.css";
 
 export default function Root(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toggleLoggedIn = () => setIsLoggedIn(!isLoggedIn);
   return (
     <div className={styles.navbar}>
       Navbar
       <div>
-        <Button>Log in</Button>
+        <Button onClick={toggleLoggedIn}>
+          {isLoggedIn ? "Log out" : "Log in"}
+        </Button>
       </div>
     </div>
   );
