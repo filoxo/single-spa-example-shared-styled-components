b5704d7893d8f5091ac3a2acc048b8d6ddfd662d
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-10 12:44:35 -0600

    Add navbar only styles

--- navbar/src/root.component.js
+++ navbar/src/root.component.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button } from "@filoxo/styleguide";
+import styles from "./root.styles.css";
 
 export default function Root(props) {
   return (
-    <div>
+    <div className={styles.navbar}>
       Navbar
       <div>
         <Button>Log in</Button>
