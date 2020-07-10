b5446df667549fae66b09d98f8a897f5d351e390
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-10 12:44:50 -0600

    Update tests

--- navbar/src/root.component.test.js
+++ navbar/src/root.component.test.js
@@ -5,6 +5,6 @@ import Root from "./root.component";
 describe("Root component", () => {
   it("should be in the document", () => {
     const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+    expect(getByText(/Navbar/i)).toBeInTheDocument();
   });
 });
