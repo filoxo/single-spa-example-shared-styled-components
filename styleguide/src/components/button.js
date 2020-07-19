70ea4a0991a787e5966b0a9d61efc31ca3fc4814
Carlos <filoxo@users.noreply.github.com>
2020-07-19 09:13:25 -0600

    Feat/implement with system builds (#1)
    * Switch to esm-build styled-components
    
    * Implement keyframes animation to demonstrate named export working correctly

--- styleguide/src/components/button.js
+++ styleguide/src/components/button.js
@@ -1,5 +1,29 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const shake = keyframes`
+  0%,
+  100% {
+    transform: translateX(0);
+  }
+  10%,
+  30%,
+  50%,
+  70% {
+    transform: translateX(-2px);
+  }
+  20%,
+  40%,
+  60% {
+    transform: translateX(2px);
+  }
+  80% {
+    transform: translateX(1px);
+  }
+  90% {
+    transform: translateX(-1px);
+  }
+`;
 
 const StyledButton = styled.button`
   background: papayawhip;
@@ -9,8 +33,9 @@ const StyledButton = styled.button`
   padding: 0.6rem 1.2rem;
 
   :focus {
-    outline: none;
+    animation: ${shake} 1s cubic-bezier(0.455, 0.03, 0.515, 0.955) both;
     box-shadow: 0 0 0 3px coral;
+    outline: none;
   }
 `;
 
